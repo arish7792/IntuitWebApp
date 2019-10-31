46b6738b71257f97a31fb99684c3d828dd135157
Arish Dhingra <arishdhingra@Arishs-MacBook-Pro.local>
2019-10-31 16:54:04 -0700

    adding docerfile for production env.

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('Running TC1', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
+it('Running TC2', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
