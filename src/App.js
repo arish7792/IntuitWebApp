ff8f4b043b7f929c09a0c31a7f976b4f64959cb6
Arish Dhingra <arishdhingra@Arishs-MacBook-Pro.local>
2019-10-31 19:38:16 -0700

    changing the message.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Intuit Interview.
+          Welcome to Intuit Interviews.
         </p>
       </header>
     </div>
