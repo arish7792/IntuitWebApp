e241d75c0d0b23218d7c335e2514e9bd451a3736
Arish Dhingra <arishdhingra@Arishs-MacBook-Pro.local>
2019-11-11 21:37:23 -0800

    changing the code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I work at Informatica currently badlyly.
+          I work at Informatica.
         </p>
       </header>
     </div>
